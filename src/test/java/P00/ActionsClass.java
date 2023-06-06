d678d4c69ac8dc8810caccbaa9d7acb3017868d2
Emre Cigit <125315547+emrecigit@users.noreply.github.com>
2023-06-06 11:18:22 +0300

    first commitv3

---
+++ src/test/java/P00/ActionsClass.java
@@ -0,0 +1,32 @@
+package P00;
+
+public class ActionsClass {
+/*
+    Actions actions = new Actions(driver);
+
+    Klavye ve mouse ile yapilan tum isleri yapmamizi saglar.
+    Mouse================================================================
+    actions.moveToElement() : Bir web elementin uzerine gidip beklememizi sagliyor.
+    actions.click() :
+    actions.clickAndHold() : Tikla ve Tut (Tutup saga sola cekmek icin)
+    actions.contextClick() : Sag click
+    actions.doubleClick() : Double click
+    actions.dragAndDrop() : Istedigimiz bir WebElementi baska bir elementin uzerine birakir
+    actions.dragAndDropBy() : Istedigimiz bir WebElementi baska bir elementin uzerine birakir (x ve y degeri ile)
+    Klavye================================================================
+    A_Istedigimiz tusa tek seferlik basma;
+    actions.sendKeys(Keys.ENTER)
+    Orn:action.click(WebElement).
+    .sendKeys("Ahmet")
+    .sendKeys(Keys.TAB)
+    .sendKeys(Keys.ENTER)
+    .perform();  // actions ile baslayan method perform ile bitmezse method calismaz
+    B_Istedigimiz tusa istedigimiz surece basili tutma
+    actions.KeyDown() Ornegin Shift'e basili tutma
+    actions.KeyUp() Basili Tusu serbest birakmak icin
+
+
+
+
+ */
+}
